d7f4534d5e2a6d0cfc08126c119f26743ed340e9
Dimitri Araz <dimitri.araz@gmail.com>
2023-09-03 14:14:02 +0200

    Add first 5 bonus functions

---
+++ libft/ft_lstsize.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daraz <daraz@student.42prague.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/03 13:21:15 by daraz             #+#    #+#             */
+/*   Updated: 2023/09/03 13:25:29 by daraz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_lstsize measures the size of a given list by counting
+	the number of nodes in it.
+
+	RETURN VALUE :
+	The integer number of nodes in the given list.
+*/
+
+int	ft_lstsize(t_list *lst)
+{
+	int	i;
+
+	i = 0;
+	while (lst)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i);
+}
