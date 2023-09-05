41b25bb9b50d5e180b39f2fb1db6fa951b7144e1
Dimitri Araz <dimitri.araz@gmail.com>
2023-09-05 07:55:08 +0200

    Complete bonus part

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daraz <daraz@student.42prague.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 08:54:10 by daraz             #+#    #+#             */
+/*   Updated: 2023/09/04 10:37:25 by daraz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_lstclear deletes each node of a list with the function
+	passed as parameter. It also frees the memory of each node and finally
+	sets the list pointer to NULL.
+
+	RETURN VALUE :
+	None.
+*/
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	if (!lst)
+		return ;
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = tmp;
+	}
+	*lst = NULL;
+}
---
+++ libft/ft_lstiter.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daraz <daraz@student.42prague.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 10:38:59 by daraz             #+#    #+#             */
+/*   Updated: 2023/09/04 10:40:25 by daraz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	DESCRIPTION :
+	The function ft_lstiter applies the function f passed as parameter
+	to the content of each node of a given list.
+
+	RETURN VALUE :
+	None.
+*/
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!f || !lst)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
