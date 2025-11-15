b2d8c87a6890579cc9318ec3e4cd2e5bbf3c6401
Dimitri Araz <128155906+DiAraz@users.noreply.github.com>
2025-11-15 17:22:36 +0100

    Delete ft_transcendence directory

--- ft_transcendence/nginx/var/www/html/components/utils/translate.js
+++
@@ -1,8 +0,0 @@
-// utils/translate.js
-import { getTranslation } from './translations.js';
-import { getLanguage } from './state.js';
-
-export function translate(word) {
-    const language = getLanguage();
-    return getTranslation(language, word);
-}
